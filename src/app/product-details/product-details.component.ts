834d809e171afc26d048cdcb45136386abc01463
afolabiopakunle <opakunleafolabi@gmail.com>
2021-09-03 00:53:01 +0100

    Activating Routes

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { Product, products } from "../products";
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
+
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
 
 }
